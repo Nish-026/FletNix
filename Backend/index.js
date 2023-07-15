1cc47f7da8708210dd1f0ce562565dad6811b2ee
Nish-026 <nishtha.kashyap2608@gmail.com>
2023-07-16 01:09:07 +0530

    Added header

--- Backend/index.js
+++ Backend/index.js
@@ -12,6 +12,13 @@ app.use(cors({
     origin: 'http://localhost:4200'
   }));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); // Replace with your frontend URL
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.get("/",(req,res)=>{
     res.send({"message":"Welcome to Fletnix"})
 })
