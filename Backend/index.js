5f0ca397ec48ecd046e41eb641aca98871f48a02
Nish-026 <nishtha.kashyap2608@gmail.com>
2023-07-16 01:18:57 +0530

    Fixed access control

--- Backend/index.js
+++ Backend/index.js
@@ -13,8 +13,8 @@ app.use(cors({
   }));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); // Replace with your frontend URL
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Replace with your frontend URL
+    res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
