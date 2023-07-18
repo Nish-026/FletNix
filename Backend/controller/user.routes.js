c22cd4f735985997b46b03fc6d689dcf48fab3c2
Nish-026 <nishtha.kashyap2608@gmail.com>
2023-07-18 15:00:56 +0530

    Updated signup route response

--- Backend/controller/user.routes.js
+++ Backend/controller/user.routes.js
@@ -24,10 +24,10 @@ userRouter.post('/signup', async (req, res) => {
     const user = new User({ username,email, password: hashed_password,age});
     await user.save();
 
-    res.status(200).json({ message: 'User created successfully' });
+    res.status(200).json({ status:200,message: 'User created successfully' });
   } catch (error) {
     console.log(error)
-    res.status(500).json({ message: 'Something went wrong' });
+    res.status(500).json({ status:500,message: 'Something went wrong' });
   }
 });
 
